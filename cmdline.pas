657efaf5e294c599373a25f1be08f046bb2ccf0b
alexa <alexander_sumaneev@hotmail.com>
2017-04-06 12:19:41 +0300

    cvc

--- cmdline.pas
+++ cmdline.pas
@@ -1,6 +1,6 @@
 (*
  * Project: lab6
- * User: alexa
+ * User: alexander_sumaneev
  * Date: 05.04.2017
  *)
 unit cmdline;
@@ -9,28 +9,36 @@ interface
 
 type cmd_line = object
 private
-        str: string;
-        cmd:string;
-        number:Integer;
-        i_cmd:Integer;
-        i_fill:Integer;
-        i_arrow:Integer;
-        cmd_list: array[1..5] of string;
-        procedure arrow_up;
-        procedure enter;
-        procedure tab;
-        procedure key_press;
-        procedure del_spaces;
-        function validation:Boolean;
+        str: string; //строка на экране
+        cmd:string; //команда после разбиения строки
+        number:Integer; //аргумент команды
+        i_cmd:Integer; // номер команды в i_cmd_list
+        i_fill:Integer; // количество заполненных элементов в i_cmd_list
+        i_arrow:Integer; // номер команды в i_cmd_list при нажатии стрелки вверх
+        cmd_list: array[0..4] of string; //список доступных команд
+        i_cmd_list: array[0..4] of string;//список 5 последних введенных команд
+        procedure arrow_up; //действия при нажатии стрелки вверх
+        procedure enter; // при нажатии enter
+        procedure tab; //автодополнение команды при нажатии tab
+        procedure key_press; //обработка нажатия клавиш
+        procedure del_spaces; //удаление лишних пробелов
+        procedure help; //вывод справки
+        function validation:Boolean; //проверка команды на корректность
 
 public
         constructor create;
-        procedure init;
+        procedure init; //запуск меню
 end;
 
 implementation
 
 uses bintree,crt,regexpr;
+
+procedure cmd_line.help();
+begin
+
+end;
+
 
 function cmd_line.validation():Boolean;
 begin
@@ -59,11 +67,14 @@ end;
 
 constructor cmd_line.create ;
 begin
-    cmd_list[1]:='help';
-    cmd_list[2]:='insert';
-    cmd_list[3]:='print';
-    cmd_list[4]:='find';
-    cmd_list[5]:='delete';
+    cmd_list[0]:='help';
+    cmd_list[1]:='insert';
+    cmd_list[2]:='print';
+    cmd_list[3]:='find';
+    cmd_list[4]:='delete';
+    i_arrow:=0;
+    i_cmd:=0;
+    i_fill:=0;
 
 end;
 
